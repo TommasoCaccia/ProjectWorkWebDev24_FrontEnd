03ae8eec24e83815058e673e0e8abd919e541449
Tommaso Caccia <tommaso.caccia@itsincom.it>
2024-03-25 09:18:28 +0100

    Aggiunta funzionalità per tema chiaro/scuro su tutto il sito sistemato css in tutte le pagine

--- Script/Tema.js
+++ Script/Tema.js
@@ -1,11 +1,22 @@
-function toggleTheme() {
-    const themeLink = document.getElementById('theme-link');
-    // Controlla se il tema corrente è il chiaro o lo scuro
-    if (themeLink.getAttribute('href') === '../Script/LightTheme.css') {
-        // Cambia al tema scuro
-        themeLink.setAttribute('href', '../Script/DarkTheme.css');
-    } else {
-        // Cambia al tema chiaro
-        themeLink.setAttribute('href', '../Script/LightTheme.css');
-    }
+const themeToggle = document.getElementById('theme-toggle');
+const body = document.body;
+const themeLink = document.getElementById('theme');
+
+themeToggle.addEventListener('click', function() {
+if (body.classList.contains('light-theme')) {
+// Cambia al tema scuro
+body.classList.remove('light-theme');
+body.classList.add('dark-theme');
+themeLink.setAttribute('href', '../Styles/DarkTheme.css');
+} else {
+// Cambia al tema chiaro
+body.classList.remove('dark-theme');
+body.classList.add('light-theme');
+themeLink.setAttribute('href', '../Styles/Lettera.css');
 }
+
+});
+
+
+
+
