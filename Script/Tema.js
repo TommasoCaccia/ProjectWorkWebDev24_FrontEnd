68698db20cd6dad28f7f731530ab41c8adeaf4fb
Tommaso Caccia <tommaso.caccia@itsincom.it>
2024-03-26 09:40:14 +0100

    Sistemata funzionalità cambio tema sono state rese responsive le pagine del login, dipendenti e invio lettera

--- Script/Tema.js
+++ Script/Tema.js
@@ -2,55 +2,34 @@ const themeToggle = document.getElementById('theme-toggle');
 const body = document.body;
 const themeLink = document.getElementById('theme');
 let themePreference = localStorage.getItem('themePreference');
- 
+
 // Imposta il tema predefinito come chiaro se non c'è nessuna preferenza salvata
 if (!themePreference) {
-    themePreference = 'light';
+  themePreference = 'light';
 }
- 
+
 // Imposta il tema in base alla preferenza salvata
 if (themePreference === 'dark') {
+  body.classList.remove('light-theme');
+  body.classList.add('dark-theme');
+  themeLink.setAttribute('href', '../Styles/DarkTheme.css');
+  themeToggle.checked = true;
+}
+
+// Aggiungi un event listener per cambiare il tema quando l'utente clicca sul toggle
+themeToggle.addEventListener('click', function() {
+  if (body.classList.contains('light-theme')) {
     body.classList.remove('light-theme');
     body.classList.add('dark-theme');
     themeLink.setAttribute('href', '../Styles/DarkTheme.css');
-}
- 
-// Aggiungi un event listener per cambiare il tema quando l'utente clicca sul toggle
-themeToggle.addEventListener('click', function() {
-    if (body.classList.contains('light-theme')) {
-        body.classList.remove('light-theme');
-        body.classList.add('dark-theme');
-        themeLink.setAttribute('href', '../Styles/DarkTheme.css');
-        localStorage.setItem('themePreference', 'dark'); // Salva la preferenza del tema
-    } else {
-        body.classList.remove('dark-theme');
-        body.classList.add('light-theme');
-        themeLink.setAttribute('href', '../Styles/Lettera.css');
-        localStorage.setItem('themePreference', 'light'); // Salva la preferenza del tema
-    }
+    localStorage.setItem('themePreference', 'dark'); // Salva la preferenza del tema
+  } else {
+    body.classList.remove('dark-theme');
+    body.classList.add('light-theme');
+    themeLink.setAttribute('href', '../Styles/Lettera.css');
+    localStorage.setItem('themePreference', 'light'); // Salva la preferenza del tema
+  }
 });
-
-
-
-/*const themeToggle = document.getElementById('theme-toggle');
-const body = document.body;
-const themeLink = document.getElementById('theme');
-
-themeToggle.addEventListener('click', function() {
-if (body.classList.contains('light-theme')) {
- //Cambia al tema scuro
-body.classList.remove('light-theme');
-body.classList.add('dark-theme');
-themeLink.setAttribute('href', '../Styles/DarkTheme.css');
-} else {
-// Cambia al tema chiaro
-body.classList.remove('dark-theme');
-body.classList.add('light-theme');
-themeLink.setAttribute('href', '../Styles/Lettera.css');
-}
-
-});*/
-
 
 
 
